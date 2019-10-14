c4a486f7ce0806441aaba0a8766f1e95023d23ec
jay940059 <44132401+jay940059@users.noreply.github.com>
2019-10-14 18:29:18 +0800

    Delete PalindromeNumber.py

--- PalindromeNumber.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self,x):
-        if x<0: #如果是負數就不會是倒數
-            return(False)
-        else:
-            if str(x) == str(x)[::-1]: #如果是正數倒數就會跟自己相等
-                return(True)
-            else:
-                return(False)
