b3531e3e3833589a8d55bcd20aaeb0c805018703
jay940059 <44132401+jay940059@users.noreply.github.com>
2019-10-14 18:29:27 +0800

    Delete TwoSum.py

--- TwoSum.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)): #第一個迴圈只拿一個數字
-            for j in range(i+1,len(nums)):#第二個迴圈拿所有數字跟第一個得做比較
-                if nums[i]+nums[j]==target:
-                    return [i,j]
